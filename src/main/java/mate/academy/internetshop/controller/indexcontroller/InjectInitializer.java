908ae45a099b0df9c765474a5c0064aac525caf5
SergiyAgeev <sergiyageev@gmail.com>
2020-01-14 14:34:05 +0200

    log4j

--- src/main/java/mate/academy/internetshop/controller/indexcontroller/InjectInitializer.java
+++ src/main/java/mate/academy/internetshop/controller/indexcontroller/InjectInitializer.java
@@ -4,13 +4,18 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import mate.academy.internetshop.lib.Injector;
+import org.apache.log4j.Logger;
 
 public class InjectInitializer implements ServletContextListener {
+    private static final Logger LOGGER = Logger.getLogger(InjectInitializer.class);
+
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         try {
+            LOGGER.info("try to DI");
             Injector.injectDependency();
         } catch (IllegalAccessException e) {
+            LOGGER.error("Problem with initialization of dependencies", e);
             throw new RuntimeException(e);
         }
     }
